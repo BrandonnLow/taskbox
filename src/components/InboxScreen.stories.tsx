6e4ec63dd41b54e3befe3442ced42f7d8910ee7c
Brandon Low <lssb2003@gmail.com>
2026-04-01 17:16:06 +0800

    GitHub action setup

--- src/components/InboxScreen.stories.tsx
+++ src/components/InboxScreen.stories.tsx
@@ -1,9 +1,11 @@
 
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
- import { http, HttpResponse } from 'msw';
+ import { waitFor, waitForElementToBeRemoved } from 'storybook/test';
 
- import { MockedState } from './TaskList.stories';
+import { http, HttpResponse } from 'msw';
+
+import { MockedState } from './TaskList.stories';
 
 import { Provider } from 'react-redux';
 
@@ -22,27 +24,38 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
- parameters: {
-   msw: {
-     handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-         return HttpResponse.json(MockedState.tasks);
-       }),
-     ],
-   },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+ play: async ({ canvas, userEvent }) => {
+   // Waits for the component to transition from the loading state
+   await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+   // Waits for the component to be updated based on the store
+   await waitFor(async () => {
+     // Simulates pinning the first task
+     await userEvent.click(canvas.getByLabelText('pinTask-1'));
+     // Simulates pinning the third task
+     await userEvent.click(canvas.getByLabelText('pinTask-3'));
+   });
  },
 };
 
 export const Error: Story = {
- parameters: {
-   msw: {
-     handlers: [
-       http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-         return new HttpResponse(null, {
-           status: 403,
-         });
-       }),
-     ],
-   },
- },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+          return new HttpResponse(null, {
+            status: 403,
+          });
+        }),
+      ],
+    },
+  },
 };
